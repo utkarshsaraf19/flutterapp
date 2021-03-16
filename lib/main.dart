2817089554959f1b69e47950c8ad7d663bef8062
utkarsh.saraf <utkarsh.saraf19@gmail.com>
2021-03-16 20:04:32 +0530

    This is First Commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// The main function is starting point of all flutter apps
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text("Hello World"),
+      ),
+    ),
+  );
+}
