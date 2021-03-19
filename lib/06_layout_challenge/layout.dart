f25bfde98492bb5eee46ffc107960096e7bc5297
utkarshsaraf19 <utkarsh.saraf19@gmail.com>
2021-03-19 11:56:33 +0530

    layout challenge

--- lib/06_layout_challenge/layout.dart
+++ lib/06_layout_challenge/layout.dart
@@ -12,21 +12,33 @@ class MyLayoutApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
-                height: 100,
                 width: 100,
                 color: Colors.amber,
               ),
               Container(
-                height: 100,
                 width: 100,
-                color: Colors.blue,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      width: 100,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      width: 100,
+                      color: Colors.yellow[200],
+                    ),
+                  ],
+                ),
               ),
               Container(
-                height: 100,
                 width: 100,
                 color: Colors.lightGreen,
+                alignment: Alignment.topLeft,
               ),
             ],
           ),
