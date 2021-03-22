ef705a967f5f80cfb86cb3517f1847bf7e49c820
utkarsh.saraf <utkarsh.saraf19@gmail.com>
2021-03-22 17:27:21 +0530

    mi card challenge

---
+++ lib/07_mi_card_app/mi_card.dart
@@ -0,0 +1,38 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MiCardApp());
+}
+
+class MiCardApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('images/diamond.png'),
+            ),
+            Text(
+              "Diamond Saraf",
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.white,
+                fontFamily: 'Pacifico',
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        )),
+      ),
+    );
+  }
+}
